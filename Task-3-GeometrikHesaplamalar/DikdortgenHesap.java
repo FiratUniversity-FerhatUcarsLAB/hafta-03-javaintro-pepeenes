b753e60ebe45104e4bc85b84e634a15ffe1556bf
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 17:27:01 +0300

    Update DikdortgenHesap.java

--- Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -1,18 +1,19 @@
 public class DikdortgenHesap {
     static void main(String[] args) {
         //Atanan değerler
-        int kısaKenar = 4.5;
-        int uzunKenar = 7.9;
+        double kısaKenar = 4.5;
+        double uzunKenar = 7.9;
 
 
         //Çevre hesaplama
-        int cevre = (uzunKenar + kısaKenar)*2;
+        double cevre = (uzunKenar + kısaKenar)*2;
         System.out.println("Dikdörtgenin çevresi : " + cevre);
 
         //Alan hesaplama
 
-        int alan = uzunKenar * kısaKenar;
+        double alan = uzunKenar * kısaKenar;
         System.out.println("Dikdörtgenin alanı : " + alan);
     }
 }
+
 
