4e73709b0a48256bc8222743ed4ec601c132da97
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 19:03:58 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,14 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+
+        String isim = "elma";
+        int miktar = 5;
+        int fiyat = 10;
+        int toplam_fiyat = 5*10;
+                
+        System.out.println("Urun" + "    " + "miktar" + "    " + "fiyat" +"    " +  "toplam_fiyat");
+        System.out.println("-----------------------------------------");
+        System.out.println(isim + "      " + miktar +"        " + fiyat + "         " + toplam_fiyat);
+
+    }
+}
