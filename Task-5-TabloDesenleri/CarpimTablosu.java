8a1405f6f4f51432ff71533e448c8f15dbdacccb
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 18:13:12 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,16 @@
+public class CarpimTablosu {
+    public static void main(String[] args) {
+        
+        System.out.println("1 x 2 =" + 1*2);
+        System.out.println("2 x 2 =" + 2*2);
+        System.out.println("3 x 2 =" + 3*2);
+        System.out.println("4 x 2 =" + 4*2);
+        System.out.println("5 x 2 =" + 5*2);
+        System.out.println("6 x 2 =" + 6*2);
+        System.out.println("7 x 2 =" + 7*2);
+        System.out.println("8 x 2 =" + 8*2);
+        System.out.println("9 x 2 =" + 9*2);
+        System.out.println("10 x 2 =" + 10*2);
+        
+    }
+}
