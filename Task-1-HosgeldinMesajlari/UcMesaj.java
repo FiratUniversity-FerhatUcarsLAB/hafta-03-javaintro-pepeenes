f659ed212272b7e893b702821a8d092cf703090f
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 17:01:28 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    static void main(String[] args) {
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
