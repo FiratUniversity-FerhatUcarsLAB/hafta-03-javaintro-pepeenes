3c230a4297dcc794323a913fc1b9648c2b4e29f4
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 18:10:50 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,26 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+        System.out.println("Mil ------> Kilometre");
+        System.out.println("-----------------------");
+
+        int mil_1 = 1;
+        int km_1 = mil_1 * 1609;
+
+        int mil_2 = 5;
+        int km_2 = mil_2 * 1609;
+
+        int mil_3 = 10;
+        int km_3 = mil_3 * 1609;
+
+        int mil_4 = 20;
+        int km_4 = mil_4 * 1609;
+
+        int mil_5 = 50;
+        int km_5 = mil_5 * 1609;
+        System.out.println(mil_1 + "            " + km_1);
+        System.out.println(mil_2 + "            " + km_2);
+        System.out.println(mil_3 + "            " + km_3);
+        System.out.println(mil_4 + "            " + km_4);
+        System.out.println(mil_5 + "            " + km_5);
+    }
+}
