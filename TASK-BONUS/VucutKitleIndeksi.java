427867f303fbc927d3c75bb851f6ab0c901e8fdd
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 19:10:46 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,9 @@
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+
+        int boy = 185;
+        int kilo = 70 ;
+        double BMI =  kilo / (Math.pow(boy,2));
+        System.out.println("BMI degeriniz : " + BMI );
+    }
+}
