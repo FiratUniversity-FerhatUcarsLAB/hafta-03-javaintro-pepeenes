4c276c5b2ef40a42d4b3fc2a27aa61500c649c38
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 17:58:58 +0300

    Update AsciiArtHarf.java

--- Task-4-StringDesenler/AsciiArtHarf.java
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -1,1 +1,24 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        char c1 = 'E';
+        char c2 = 'n';
+        char c3 = 'e';
+        char c4 = 's';
+        char c5 = ' ';
+        char c6 = 'P';
+        char c7 = 'e';
+        char c8 = 'p';
+        char c9 = 'e';
+
+        System.out.println(c1 + " = " + (int)c1);
+        System.out.println(c2 + " = " + (int)c2);
+        System.out.println(c3 + " = " + (int)c3);
+        System.out.println(c4 + " = " + (int)c4);
+        System.out.println(c5 + " = " + (int)c5);
+        System.out.println(c6 + " = " + (int)c6);
+        System.out.println(c7 + " = " + (int)c7);
+        System.out.println(c8 + " = " + (int)c8);
+        System.out.println(c9 + " = " + (int)c9);
+    }
+}
 
