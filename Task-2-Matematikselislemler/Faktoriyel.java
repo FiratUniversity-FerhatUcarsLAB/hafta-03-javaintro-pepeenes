21a839de1c8fda353ca733371b1dd934e2161e5f
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 18:23:29 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,11 @@
+public class Faktoriyel {
+    public static void main(String[] args) {
+        //Atanmış değer
+        int a = 5 ;
+
+        //Faktöriyel hesabı
+
+        System.out.println("5! = 5 x 4 x 3 x 2 x 1 = " + a*(a-1)*(a-2)*(a-3)*(a-4));
+
+    }
+}
