e281e79176a113f41c3a1a3d49adfdc998e373b0
Enes PEPE <100033148+pepeenes@users.noreply.github.com>
2025-10-22 17:52:43 +0300

    Update AsciiArtHarf.java

--- Task-4-StringDesenler/AsciiArtHarf.java
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -1,9 +1,1 @@
-class AsciiArtHarf {
-    public static void main(String[] args) {
-        double pay = (9.5 * 4.5) - (2.5 * 3);
-        double payda = 45.5 - 3.5;
-        double sonuc = pay / payda;
 
-        System.out.printf("%.4f", sonuc);
-    }
-}
